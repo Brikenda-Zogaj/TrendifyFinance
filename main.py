79a6ea4ac1e61cae95b017eb0998663718a781dc
Brikenda-Zogaj <brikenda.zogaj@student.uni-pr.edu>
2025-02-08 19:14:52 +0100

    minor changes

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from src.data_loader import load_data
 from src.exploratory_analysis import explore_data, save_clean_data
+from src.visualization import plot_expense_trend
+
+
 
 if __name__ == "__main__":
     file_path = "data/myExpenses1.csv"  # Change this to your dataset path
     df = load_data(file_path)
     explore_data(df)
     save_clean_data(df)
+    plot_expense_trend(df)
+
