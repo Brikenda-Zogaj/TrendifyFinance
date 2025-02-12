9c8cf5bdb25bb2f827d4942bbe88a5d67fe88b27
Brikenda-Zogaj <brikenda.zogaj@student.uni-pr.edu>
2025-02-12 16:11:17 +0100

    Minor changes

--- app.py
+++ app.py
@@ -6,3 +6,9 @@ st.sidebar.title("📍 Navigation")
 options = ["📊 View Expenses", "🔮 Predict Expenses"]
 choice = st.sidebar.radio("Go to:", options)
 
+st.subheader("📊 Your Expense Data")
+st.write(df.tail(10))  # Shfaq vetëm 10 të fundit
+
+
+st.subheader("📈 Expense Trend")
+plot_expense_trend(df)
